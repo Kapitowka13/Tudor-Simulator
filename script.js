8c7ba0a37a9d19893ae9d1df6271baba407f9984
Max <82305071+Kapitowka13@users.noreply.github.com>
2022-04-17 19:02:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -22,17 +22,20 @@ const stFood = document.querySelector('.statFood');
 const btnGoFood = document.getElementById('goFood');
 const btnGoHigh = document.getElementById('goHigh');
 const btnGoShit = document.getElementById('goShit');
+
+const btnGoKlad = document.getElementById('goKlad');
+const btnGoDeal = document.getElementById('goDeal');
+const btnGoShop = document.getElementById('goShop');
+const btnGoHome = document.getElementById('goHome');
 //Кнопки
 
-function goShit() {
-    ++weed;
-    stWeed.innerHTML = weed;
-    information.innerHTML = `Рєшнув бошку, красава.`;
-    food -= 4;
-    stFood.innerHTML = food;
+function restart() {
     if (food <= 0) {
         information.innerHTML = `ТИ ЗДОХ ОТ ГОЛОДА ДОВБОЙОБ! Граєм по новому <br> <br>
          Ти бухав всю ніч. Ти Тудор і тобі дуже хуйово.<br> Ти намацав у себе в кармані пятку, вже непогано!`;
+
+         firstButtons();
+
         weed = 1;
         stWeed.innerHTML = weed;
         high = 0;
@@ -40,9 +43,53 @@ function goShit() {
         food = 80;
         stFood.innerHTML = food;
     }
+} //РЕСТАРТ ИГРЫ
+function ifHigh() {
+    if (high >= 100) {
+        information.innerHTML = `Ти поймав блєдного. Гра пройдена!`;
+
+    document.getElementsByClassName('button')[0].style= "display: none";
+    document.getElementsByClassName('button')[1].style= "display: none";
+    document.getElementsByClassName('button')[2].style= "display: none";
+    document.getElementsByClassName('button2')[0].style= "display: none";
+    document.getElementsByClassName('button2')[1].style= "display: none";
+    document.getElementsByClassName('button2')[2].style= "display: none";
+    }
+}//Проверка на high
+function firstButtons() {
+    document.getElementsByClassName('button')[0].style= "display: table";
+    document.getElementsByClassName('button')[1].style= "display: table";
+    document.getElementsByClassName('button')[2].style= "display: table";
+    document.getElementsByClassName('button2')[0].style= "display: none";
+    document.getElementsByClassName('button2')[1].style= "display: none";
+    document.getElementsByClassName('button2')[2].style= "display: none";
+    document.getElementsByClassName('button2')[3].style= "display: none";
+}//ВИДНО ТОЛЬКО ПЕРВЫЕ ТРИ КНОПКИ
+function secondButtons() {
+    document.getElementsByClassName('button')[0].style= "display: none";
+    document.getElementsByClassName('button')[1].style= "display: none";
+    document.getElementsByClassName('button')[2].style= "display: none";
+    document.getElementsByClassName('button2')[0].style= "display: table";
+    document.getElementsByClassName('button2')[1].style= "display: table";
+    document.getElementsByClassName('button2')[2].style= "display: table";
+    document.getElementsByClassName('button2')[3].style= "display: table";
+}//ВИДНО СЛЕДУЮЩИЕ ЧЕТЫРЕ КНОПКИ
+
+function goHome() {
+    information.innerHTML = `Ти прийшов додому`;
+
+    firstButtons();
+}
+
+function goShit() {
+    --food;
+    stFood.innerHTML = food;
+    information.innerHTML = `Ти вийшов на вулицю. Шо думаєш робить далі, куди підем?`;
+
+    secondButtons();
 }
 function goHigh() {
-    if (weed < 100 && weed) {
+    if (weed) {
         --weed;
         high += Math.ceil(Math.random() * 10);
         stHigh.innerHTML = high;
@@ -51,16 +98,7 @@ function goHigh() {
 
         food -= 10;
         stFood.innerHTML = food;
-        if (food <= 0) {
-            information.innerHTML = `ТИ ЗДОХ ОТ ГОЛОДА ДОВБОЙОБ! Граєм по новому <br> <br>
-             Ти бухав всю ніч. Ти Тудор і тобі дуже хуйово.<br> Ти намацав у себе в кармані пятку, вже непогано!`;
-            weed = 1;
-            stWeed.innerHTML = weed;
-            high = 0;
-            stHigh.innerHTML = high;
-            food = 80;
-            stFood.innerHTML = food;
-        }
+        restart();
     } else {
         information.innerHTML = `Шо ти тягнуть будеш? В тебе нема нічого, дурло!`;
     }
@@ -84,8 +122,37 @@ function goFood() {
         information.innerHTML = `Ти вже плотно похавав, хвате`;
     }
 }
+function goKlad() {
+    ++weed;
+    stWeed.innerHTML = weed;
+    information.innerHTML = `Ти найшов в заброшкі священний коробок (+1 weed  -4 hungry)`;
+    food -= 4;
+    stFood.innerHTML = food;
+    restart();
+}
+function goDeal() {
+    weed += 3;
+    stWeed.innerHTML = weed;
+    information.innerHTML = `Snoop Dogg дав тобі нігерський блант (+3 weed  -15 hungry)`;
+    food -= 15;
+    stFood.innerHTML = food;
+    restart();
+}
+function goShop() {
+    weed += 10;
+    stWeed.innerHTML = weed;
+    information.innerHTML = `Ти купив пачку бошок. Хто зна за які гроші (+10 weed  -50 hungry)`;
+    food -= 50;
+    stFood.innerHTML = food;
+    restart();
+}
 
 
 btnGoFood.addEventListener("click", goFood);
 btnGoHigh.addEventListener("click", goHigh);
 btnGoShit.addEventListener("click", goShit);
+
+btnGoKlad.addEventListener("click", goKlad);
+btnGoDeal.addEventListener("click", goDeal);
+btnGoShop.addEventListener("click", goShop);
+btnGoHome.addEventListener("click", goHome);
