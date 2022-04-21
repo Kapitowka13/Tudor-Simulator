d204d86eb905538de626d86323402f3c9b63b8c9
Max <82305071+Kapitowka13@users.noreply.github.com>
2022-04-21 10:03:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let weed = 1;
 let high = 0;
 let food = 80;
 let money = 32;
-let dolg = 0;
 let sell;
+let burian = 90;
+let days = 29;
 
 //Информация
 const information = document.getElementById('information');
@@ -21,6 +22,7 @@ const btnGoFood = document.getElementById('goFood');
 const btnGoHigh = document.getElementById('goHigh');
 const btnGoShit = document.getElementById('goShit');
 const btnGoHust = document.getElementById('goHust');
+const btnGoBurr = document.getElementById('goBurr');
 
 const btnGoKlad = document.getElementById('goKlad');
 const btnGoDeal = document.getElementById('goDeal');
@@ -28,10 +30,11 @@ const btnGoShop = document.getElementById('goShop');
 const btnGoHome = document.getElementById('goHome');
 //Кнопки
 
+const burrInfo = document.getElementById('burrInfo'); // Шкала сорняка
+
 function restart() {
     if (food <= 0) {
-        information.innerHTML = `ТИ ЗДОХ ОТ ГОЛОДА ДОВБОЙОБ! Граєм по новому <br> <br>
-        Ти бухав всю ніч. Ти Тудор і тобі дуже хуйово. 
+        information.innerHTML = `ТИ ЗДОХ ОТ ГОЛОДА ДОВБОЙОБ! Граєм по новому <br>
         Ти намацав у себе в кармані пятку і пару гривнів на рево, вже непогано!`;
 
          firstButtons();
@@ -44,6 +47,8 @@ function restart() {
         stFood.innerHTML = food;
         money = 32;
         stMoney.innerHTML = money;
+        burian = 0;
+        ifBurr();
     }
 } //РЕСТАРТ ИГРЫ
 function ifHigh() {
@@ -53,16 +58,82 @@ function ifHigh() {
     document.getElementsByClassName('button')[0].style= "display: none";
     document.getElementsByClassName('button')[1].style= "display: none";
     document.getElementsByClassName('button')[2].style= "display: none";
+    document.getElementsByClassName('button')[3].style= "display: none";
+    document.getElementsByClassName('button')[4].style= "display: none";
     document.getElementsByClassName('button2')[0].style= "display: none";
     document.getElementsByClassName('button2')[1].style= "display: none";
     document.getElementsByClassName('button2')[2].style= "display: none";
+    document.getElementsByClassName('button2')[3].style= "display: none";
     }
 }//Проверка на high
+function ifBurr() {
+    switch (burian) {
+        case 10:
+            document.getElementById('burrInfo').style="width: 10%";
+            break;
+        case 20:
+            document.getElementById('burrInfo').style= "width: 20%";
+            break;
+        case 30:
+            document.getElementById('burrInfo').style= "width: 30%";
+            break; 
+        case 40:
+            document.getElementById('burrInfo').style= "width: 40%";
+            break;
+        case 50:
+            document.getElementById('burrInfo').style= "width: 50%";
+            break;
+        case 60:
+            document.getElementById('burrInfo').style= "width: 60%";
+            break;
+        case 70:
+            document.getElementById('burrInfo').style= "width: 70%";
+            break;
+        case 80:
+            document.getElementById('burrInfo').style= "width: 80%";
+            break;
+        case 90:
+            document.getElementById('burrInfo').style= "width: 90%";
+            break;
+        case 100:
+            document.getElementById('burrInfo').style= "width: 100%";
+            break;
+        default:
+            document.getElementById('burrInfo').style= "width: 0%";
+            break;
+    }
+}//Проверка на burr
+function ifDays() {
+    days += 0.2;
+    if (days >= 30) {
+        if(burian < 100) {
+            information.innerHTML = `Батьки приїхали і дали пизди за те шо город не убраний!<br>Ти здох от крепкого ляща. Граємо по новому!`;
+            firstButtons();
+
+            weed = 1;
+            stWeed.innerHTML = weed;
+            high = 0;
+            stHigh.innerHTML = high;
+            food = 80;
+            stFood.innerHTML = food;
+            money = 32;
+            stMoney.innerHTML = money;
+            burian = 0;
+            ifBurr();
+            days = 0;
+        } else {
+            information.innerHTML = `Батьки приїхали і не дали пизди. Празнуєм!!!Тепер треба думать як c'їбаця нахуй звідси<br>+200<img id="miniMoney" src="money.png">`;
+            money +=200;
+            stMoney.innerHTML = money;
+        }
+    }
+}//Проверка days
 function firstButtons() {
     document.getElementsByClassName('button')[0].style= "display: table";
     document.getElementsByClassName('button')[1].style= "display: table";
     document.getElementsByClassName('button')[2].style= "display: table";
     document.getElementsByClassName('button')[3].style= "display: table";
+    document.getElementsByClassName('button')[4].style= "display: table";
 
     document.getElementsByClassName('button2')[0].style= "display: none";
     document.getElementsByClassName('button2')[1].style= "display: none";
@@ -74,6 +145,7 @@ function secondButtons() {
     document.getElementsByClassName('button')[1].style= "display: none";
     document.getElementsByClassName('button')[2].style= "display: none";
     document.getElementsByClassName('button')[3].style= "display: none";
+    document.getElementsByClassName('button')[4].style= "display: none";
 
     document.getElementsByClassName('button2')[0].style= "display: table";
     document.getElementsByClassName('button2')[1].style= "display: table";
@@ -85,21 +157,23 @@ function secondButtons() {
 function goShit() {
     food -= 2;
     stFood.innerHTML = food;
-    information.innerHTML = `Ти вийшов на вулицю. Шо думаєш робить далі, куди підем?`;
+    information.innerHTML = `Ти вийшов на вулицю. Шо думаєш робить далі, куди підем?<br>-2<img id="miniFood" src="food.png">`;
 
     secondButtons();
 }
 function goHigh() {
     if (weed > 0) {
         --weed;
-        high += Math.ceil(Math.random() * 10);
+        h = Math.ceil(Math.random() * 10);
+        high += h;
         stHigh.innerHTML = high;
         stWeed.innerHTML = weed;
-        information.innerHTML = `Закашлявся кха кх... кхах..`;
+        information.innerHTML = `Закашлявся кха кх... кхах..<br>+${h}<img id="miniHigh" src="high.jpg">`;
 
         food -= 10;
         stFood.innerHTML = food;
         restart();
+        ifDays();
     } else if (weed < 0) {
         information.innerHTML = `Які тянуть? У тебе нема нічого ше плюс должен комусь`;
     } else {
@@ -109,19 +183,21 @@ function goHigh() {
 }
 function goFood() {
     if (food < 100) {
-        food += 10;
-        information.innerHTML = `Яка ж тіки смакота та ти шоо`;
+        food += 20;
+        information.innerHTML = `Яка ж тіки смакота та ти шоо<br> +20<img id="miniFood" src="food.png">`;
         if (food >= 100) {
             food = 100;
         }
         stFood.innerHTML = food;
 
 
-        high -=4;
+        high -=6;
         if (high < 0) {
             high = 0;
         }
         stHigh.innerHTML = high;
+
+        ifDays();
     } else {
         information.innerHTML = `Ти вже плотно похавав, хвате`;
     }
@@ -133,14 +209,14 @@ function goHust() {
             let r = Math.ceil(Math.random() * 10);
             if (r <= 4) {
                 let sellMoney = sell * 10;
-                information.innerHTML = `Ти успішно продав ${sell} грамів  (+ ${sellMoney}₴)`;
+                information.innerHTML = `Ти успішно продав ${sell} грамів <img id="miniWeed" src="weed.png"> <br> + ${sellMoney}<img id="miniMoney" src="money.png">`;
                 money += sellMoney;
                 stMoney.innerHTML = money;
                 weed -= sell;
                 stWeed.innerHTML = weed;
             } else {
                 if (money >= r) {
-                    information.innerHTML = `Тебе отпиздили за таку хуйню і забрали всю траву яку ти взяв і пару гривень. (-20 hungry -${sell} грам -${r}₴)`;
+                    information.innerHTML = `Тебе отпиздили за таку хуйню і забрали всю траву яку ти взяв і пару гривень. <br>-20<img id="miniFood" src="food.png"> -${sell}<img id="miniWeed" src="weed.png"> -${r}<img id="miniMoney" src="money.png">`;
                     weed -= sell;
                     stWeed.innerHTML = weed;
                     food -= 20;
@@ -148,7 +224,7 @@ function goHust() {
                     money -= r;
                     stMoney.innerHTML = money;
                 } else {
-                    information.innerHTML = `Тебе отпиздили за таку хуйню і забрали всю траву яку ти взяв і пару гривень. (-20 hungry -${sell} грам)`;
+                    information.innerHTML = `Тебе отпиздили за таку хуйню і забрали всю траву яку ти взяв.<br> -20<img id="miniFood" src="food.png"> -${sell}<img id="miniWeed" src="weed.png">`;
                     weed -= sell;
                     stWeed.innerHTML = weed;
                     food -= 20;
@@ -162,6 +238,30 @@ function goHust() {
         information.innerHTML = `Можна продавати лише від 1 до 5 грамів`;
     }
     restart();
+    ++days;
+    ifDays();
+}
+function goBurr() {
+    r = Math.ceil(Math.random() * 10);
+    if (r < 5) {
+        information.innerHTML = `Ти трохи поприбирав бур'ян на городі. <br>-15<img id="miniFood" src="food.png">`;
+        burian += 10;
+        food -= 15;
+        stFood.innerHTML = food;
+        ifBurr();
+    } else if (r > 5) {
+        information.innerHTML = `Ти завзято попахав на городі, але потім поняв шо це не твій огород.<br>-20<img id="miniFood" src="food.png">`;
+        food -= 20;
+        stFood.innerHTML = food;
+    } else {
+        information.innerHTML = `Поки ти роздуплявся, тебе уїбала змія за руку. <br>-20<img id="miniFood" src="food.png">`;
+        food -= 20;
+        stFood.innerHTML = food;
+    }
+    ++days;
+    ifBurr();
+    restart();
+    ifDays();
 }
 
 //////////////////////////////КНОПКИ 2
@@ -170,15 +270,17 @@ function goKlad() {
     if (r >= 5) {
         ++weed;
         stWeed.innerHTML = weed;
-        information.innerHTML = `Ти найшов в заброшкі священний коробок (+1 weed  -4 hungry)`;
-        food -= 4;
+        information.innerHTML = `Ти найшов в заброшкі священний коробок<br>+1<img id="miniWeed" src="weed.png"> -5<img id="miniFood" src="food.png">`;
+        food -= 5;
         stFood.innerHTML = food;
+        ifDays();
     } else {
         weed -= 2;
         stWeed.innerHTML = weed;
-        food -= 8;
+        food -= 10;
         stFood.innerHTML = food;
-        information.innerHTML = `Ти найшов пизди (-2 weed -8 hungry)`;
+        information.innerHTML = `Ти найшов пизди<br>-2<img id="miniWeed" src="weed.png"> -10<img id="miniFood" src="food.png">`;
+        ifDays();
     }
     restart();
 }
@@ -187,34 +289,37 @@ function goDeal() {
     if (r >= 4) {
         weed += 3;
         stWeed.innerHTML = weed;
-        information.innerHTML = `Snoop Dogg дав тобі нігерський блант (+3 weed  -15 hungry)`;
+        information.innerHTML = `Snoop Dogg дав тобі нігерський блант<br>+3<img id="miniWeed" src="weed.png"> -15<img id="miniFood" src="food.png">`;
         food -= 15;
         stFood.innerHTML = food;
     } else {
-        information.innerHTML = `Snoop Dogg вистрелив в тебе з травмата (-30 hungry)`;
+        information.innerHTML = `Snoop Dogg вистрелив в тебе з травмата<br>-30<img id="miniFood" src="food.png">`;
         food -= 30;
         stFood.innerHTML = food;
     }
+    ++days;
     restart();
+    ifDays();
 }
 function goShop() {
     if (money >= 30) {
         weed += 10;
         stWeed.innerHTML = weed;
-        information.innerHTML = `Ти купив пачку бошок за 30₴ (+10 weed)`;
+        information.innerHTML = `Ти купив пачку бошок за 30<img id="miniMoney" src="money.png"><br>+10<img id="miniWeed" src="weed.png">`;
         money -= 30;
         stMoney.innerHTML = money;
     } else {
-        information.innerHTML = `В тебе не вистачає грошей (- 10 hungry)`;
+        information.innerHTML = `В тебе не вистачає грошей. Ти розізлився, вдарив стіну і вибив собі палець<br>-10<img id="miniFood" src="food.png">`;
         food -= 10;
         stFood.innerHTML = food;
     }
     restart();
+    ifDays();
 }
 function goHome() {
     food -= 2;
     stFood.innerHTML = food;
-    information.innerHTML = `Ти прийшов додому`;
+    information.innerHTML = `Ти прийшов додому<br>-2<img id="miniFood" src="food.png">`;
 
     firstButtons();
 }
@@ -223,6 +328,7 @@ btnGoFood.addEventListener("click", goFood);
 btnGoHigh.addEventListener("click", goHigh);
 btnGoShit.addEventListener("click", goShit);
 btnGoHust.addEventListener("click", goHust);
+btnGoBurr.addEventListener("click", goBurr);
 
 btnGoKlad.addEventListener("click", goKlad);
 btnGoDeal.addEventListener("click", goDeal);
